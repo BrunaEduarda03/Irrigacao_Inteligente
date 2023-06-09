631e2af1de9fba43876c763aac9badc3d1227c31
Bruna Eduarda <brunaduda37@gmail.com>
2023-06-08 22:20:55 -0300

    Realizado troca do esp 8266 para esp 32(sensores implementados)

--- irrigacao.ino
+++ irrigacao.ino
@@ -1,30 +1,38 @@
 /* OLED */
-#include <ACROBOTIC_SSD1306.h> // library for OLED: SCL ==> D1; SDA ==> D2
-#include <SPI.h>
 #include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED width,  in pixels
+#define SCREEN_HEIGHT 64 // OLED height, in pixels
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+/*
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp8266.h>
-
+*/
 /*DHT11*/
 #include "DHT.h"
-#define DHTPIN D3  
+#define DHTPIN 4  
 #define DHTTYPE DHT11 
 
 /*-------- Token de Autenticação ----------- */
-char auth[] = "Qyvl_WEZVA4TEx0aOWgYQxdTLvplmwY2";
+//char auth[] = "Qyvl_WEZVA4TEx0aOWgYQxdTLvplmwY2";
 /*-------- Configurações de Wi-Fi ----------- */
-char ssid[] = "connect-izaias";
-char pass[] = "244466666"; 
+//char ssid[] = "Bruna";
+//char pass[] = "bruna0303"; 
 
-BlynkTimer timer;
+//BlynkTimer timer;
 DHT dht(DHTPIN, DHTTYPE);
 float airHum = 0;
 float airTemp = 0;
 
 /* Soil airHumidity */
-#define soilHumPIN 0
+#define soilHumPIN 2
 float soilHum = 0;
+
+/* rain sensor */
+#define runPIN 15
+float rainnSensor = 0;
 
 /* Automatic Control Parameters Definition 
 #define DRY_SOIL      30
@@ -34,7 +42,7 @@ float soilHum = 0;
 #define TIME_PUMP_ON  15
 #define TIME_LAMP_ON  15
 */
-
+/*
 void myTimer() 
 {
   // This function describes what will happen with each timer tick
@@ -43,11 +51,11 @@ void myTimer()
   Blynk.virtualWrite(V1,airHum);
   Blynk.virtualWrite(V2,soilHum);
 }
-
+*/
 void setup()
 {
-  Blynk.begin(auth, ssid, pass);            // Inicializa o Blynk
-  timer.setInterval(1000L, myTimer);            // Inicializa o Timer do blynk para enviar os dados para o APP
+ //Blynk.begin(auth, ssid, pass);            // Inicializa o Blynk
+  //timer.setInterval(1000L, myTimer);            // Inicializa o Timer do blynk para enviar os dados para o APP
   Serial.begin(115200);
   delay(10);
   dht.begin();
@@ -56,14 +64,14 @@ void setup()
 
 void loop()
 {
-
   // Runs all Blynk stuff
-  Blynk.run(); 
+  //Blynk.run(); 
   // runs BlynkTimer
-  timer.run(); 
+  //timer.run(); 
   getSensors();
   displayData();
-  delay(2000);
+  delay(1000);
+  
 }
 
 /***************************************************
@@ -72,29 +80,14 @@ void loop()
 void oledStart(void)
 {
   Wire.begin();  
-  oled.init();                      // Initialze SSD1306 OLED display
-  clearOledDisplay();
-  oled.clearDisplay();              // Clear screen
-  oled.setTextXY(0,0);              
-  oled.putString("Proj.Irrigacao");
+  oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  oled.clearDisplay();              // Clear screen       
+  oled.setTextColor(WHITE);    // set text color
+  oled.setCursor(0,0);       // set position to display     
+  oled.println("Proj.Irrigacao");
+  oled.display();
 }
 
-/***************************************************
- * Clear OLED Display
- **************************************************/
-void clearOledDisplay()
-{
-  oled.setFont(font8x8);
-  oled.setTextXY(0,0); oled.putString("                ");
-  oled.setTextXY(1,0); oled.putString("                ");
-  oled.setTextXY(2,0); oled.putString("                ");
-  oled.setTextXY(3,0); oled.putString("                ");
-  oled.setTextXY(4,0); oled.putString("                ");
-  oled.setTextXY(5,0); oled.putString("                ");
-  oled.setTextXY(6,0); oled.putString("                ");
-  oled.setTextXY(7,0); oled.putString("                ");
-  oled.setTextXY(0,0); oled.putString("                ");              
-}
 
 /***************************************************
  * Display data at Serial Monitora & OLED Display
@@ -106,29 +99,57 @@ void displayData(void)
   Serial.print("oC   airHumidity: ");
   Serial.print(airHum);
   Serial.println("%");
+  Serial.print("oC   souilHumidity: ");
+  Serial.print(soilHum);
+  Serial.println("%");
+
   
-  oled.setTextXY(3,0);              // Set cursor position, start of line 2
-  oled.putString("airTemp:" + String(airTemp) + "oC");
-  oled.setTextXY(5,0);              // Set cursor position, start of line 2
-  oled.putString("airHum: " + String(airHum) + "%");
-  oled.setTextXY(7,0);              // Set cursor position, start of line 2
-  oled.putString("soilHum:" + String(soilHum) + "%");
+  oled.setCursor(0,0);       // set position to display     
+  oled.println("Proj.Irrigacao");
+  oled.display();
+  //air temperature
+  oled.setTextSize(1);
+  oled.setCursor(0,20);              // Set cursor position, start of line 2
+  oled.print("airTemp: ");
+  oled.print(airTemp);
+  oled.print(" ");
+  oled.setTextSize(1);
+  oled.cp437(true);                  // Escrever o símbolo º 
+  oled.write(167);
+  oled.setTextSize(1);
+  oled.print("C");
+  oled.display();
+
+  //air Humidity
+  oled.setTextSize(1);
+  oled.setCursor(0,30);              // Set cursor position, start of line 2
+  oled.print("airHum: ");
+  oled.print(airHum);
+  oled.print(" ");
+  oled.setTextSize(1);
+  oled.print("%");
+  oled.display();
+
+  //soil Humidity
+  oled.setTextSize(1);
+  oled.setCursor(0,40);              // Set cursor position, start of line 2
+  oled.print("soilHum: ");
+  oled.print(soilHum);
+  oled.print(" ");
+  oled.setTextSize(1);
+  oled.print("%");
+  oled.display();
+  delay(100);
+  oled.clearDisplay(); 
 }
 
 void getSensors(void)
 {
-  float airTempIni = airTemp;
-  float airHumIni = airHum;
   airTemp = dht.readTemperature();
   airHum = dht.readHumidity();
-  soilHum = map(analogRead(soilHumPIN), 1023, 0, 0, 100); // %
-  if (isnan(airHum) || isnan(airTemp))   // Check if any reads failed and exit early (to try again).
-  {
-    Serial.println("Failed to read from DHT sensor!");
-    airTemp = airTempIni;
-    airHum = airHumIni;
-    return;
-  }
+  float sensorValue = analogRead(soilHumPIN);
+  soilHum = map(sensorValue, 0, 4095, 0, 100); // 0-100%
+  
 }
 /*
 0 ~300 solo seco
